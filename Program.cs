bb5971291c99f75001f9af3df7e5ac6dba7b61ca
13cha <13cha@DESKTOP-B0ONRGC>
2024-11-04 15:22:30 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ALBRIGHT_ASSIGNMENT_1_1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //1.1.1. SOLUTION:
+
+            Console.WriteLine("Please enter your name:");
+            string Name_String = Console.ReadLine();
+            Console.WriteLine("\nNow please enter your Age in decimal format:");
+            double Age_Double = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("\nFinally, please enter your address");
+            string Address_String = Console.ReadLine();
+  
+            Console.WriteLine("\nThanks! I record that your name is " + Name_String + ", you are " + Age_Double + " years old, and reside @ " + Address_String);
+
+           
+        }
+    }
+}
